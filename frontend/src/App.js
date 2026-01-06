e7354a88e7e3d2d2d2d77b407f9b0c9a0d3d35dc
jackiecosa <jackie@benincosa.com>
2026-01-06 12:44:24 -0800

    make frontend actually use react and import bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <h1 className="text-dark">Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
